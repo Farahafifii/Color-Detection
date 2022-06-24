72cf4a2f90631c03e6033325f60697aecb614e16
Farahafifii <farah-afifi@hotmail.com>
2022-06-24 23:35:37 +0200

    first draft

--- Color.py
+++ Color.py
@@ -3,33 +3,50 @@ import numpy as np
 import cv2
 import pandas as pd
 
+click = False # boolean variable to check if the mouse is clicked
 img  =  cv2.imread("testpic.jpg") # sample image to read the colors from 
 
 index  = ["color","color_name","hex","r","g","b"] 
 data = pd.read_csv("colors.csv", names = index, header = None)
 
+def call_back (event ,x,y,flags , param):
+   # call back function: when the mouse is clicked, the color of the pixel that the mouse clicked on is displayed
+    if event == cv2.EVENT_LBUTTONDOWN:
+        global r1,g1,b1 ,x_pos , y_pos 
+        click = True
+        x_pos = x
+        y_pos = y
+        b1 = int(b1)
+        g1 = int(g1)
+        r1 = int(r1)
+
+def get_color_name(r,g,b):
+    # get the color name from the csv file using the r,g,b values
+    min_color_diff = 10000
+    for i in range(len(data)):
+        difff = abs(r- int(data.loc[i,"r"])) + abs(g- int(data.loc[i,"g"])) + abs(b- int(data.loc[i,"b"]))
+        if difff < min_color_diff:
+            min_color_diff = difff
+            color_name = data.loc[i,"color_name"]
+    return color_name
+
 # create window to display the image and call the call back function when the mouse is clicked
 cv2.namedWindow("Color Detection")
 cv2.setMouseCallback("Color Detection", call_back)
 
-def call_back (event ,x,y,flags , param):
-   # call back function: when the mouse is clicked, the color of the pixel that the mouse clicked on is displayed
-    if event == cv2.EVENT_LBUTTONDOWN:
-        global r,g,b ,x_pos , y_pos ,click
-        click = True
-        x_pos = x
-        y_pos = y
-        b = int (b)
-        g = int (g)
-        r = int (r)
+cv2.destroyAllWindows()
+while (1):
+    
+    cv2.imshow("Color Detection", img)
+    if (click):
+        cv2.rectangle(img,(20,20),(750,60),(b1,g1,r1),-1)
+        text  = "Color: " + get_color_name(r1,g1,b1) + "RGB: " + str(r1) + "," + str(g1) + "," + str(b1)
+        cv2.putText(img,text,(50,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),2)
+        if r1+b1+g1 >= 600:
+            cv2.putText(img,text,(50,50),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,0),2,cv2.LINE_AA)
+        click = False
+        if cv2.waitKey(1) & 0xFF ==27:
+            break
 
-def get_color_name(r,g,b):
-    # get the color name from the csv file
-    min_color_diff = 1000
-    for i in range(len(data)):
-        difff = abs(r-data.r[i]) + abs(g-data.g[i]) + abs(b-data.b[i])
-        if difff < min_color_diff:
-            min_color_diff = difff
-            color_name = data.color_name[i]
-    return color_name
 
+print("hello world")
